48df7ebbdc4ff0175eeb67b4dd71cc7aaeb7b57f
Michael MacAskill <michael.macaskill@nzbri.org>
2021-04-28 23:21:36 +1200

    Add additional tests

--- tests/testthat/test-validity.R
+++ tests/testthat/test-validity.R
@@ -17,6 +17,18 @@ test_that('check internal validity of NHI check digits', {
     expect_equal(nhi_valid('JBX3658'), FALSE)
     expect_equal(nhi_valid('JBX3659'), FALSE)
 
+    # no digit can be added to the end of ZZZ004 to create a valid NHI:
+    expect_equal(nhi_valid('ZZZ0040', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0041', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0042', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0043', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0044', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0045', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0046', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0047', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0048', allow_test_cases = TRUE), FALSE)
+    expect_equal(nhi_valid('ZZZ0049', allow_test_cases = TRUE), FALSE)
+
     # a valid test case, but we aren't allowing test cases:
     expect_equal(nhi_valid('ZZZ0016'), FALSE)
     # the same valid test case, which we now permit:
@@ -35,5 +47,12 @@ test_that('check that single cases and vectors are handled', {
     expect_equal(length(nhi_valid('JBX3656')), 1)
     expect_equal(length(nhi_valid(c('JBX3656'))), 1)
     expect_equal(length(nhi_valid(c('JBX3656', 'JBX3657', NA))), 3)
+
+})
+
+test_that('check that the new format is handled', {
+
+    expect_equal(nhi_valid('ZZZ00AX', allow_test_cases = TRUE), TRUE)
+    expect_equal(nhi_valid('ZZZ00AY', allow_test_cases = TRUE), FALSE)
 
 })
